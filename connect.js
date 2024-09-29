e5066777be1e200155905f99b0bb5d34341d1f30
sshivanshg <sshivanshg@gmail.com>
2024-09-29 22:02:49 +0530

    Update connect.js

--- connect.js
+++ connect.js
@@ -1,15 +1,29 @@
 require('dotenv').config();  // Load environment variables from .env file
 
+// const mongoose = require('mongoose');
+
+// const connectToMongoDB = async (uri) => {
+//     try {
+//         await mongoose.connect(uri);  // No options needed
+//         console.log("Connected to MongoDB");
+//     } catch (error) {
+//         console.error("Failed to connect to MongoDB", error);
+//         throw error;  // Propagate the error
+//     }
+// };
+
+// module.exports = connectToMongoDB;
 const mongoose = require('mongoose');
 
-const connectToMongoDB = async (uri) => {
+async function connectToMongoDB() {
     try {
-        await mongoose.connect(uri);  // No options needed
-        console.log("Connected to MongoDB");
+        const uri = process.env.MONGODB_URI; // Ensure this is your Atlas URI
+        await mongoose.connect(uri, { useUnifiedTopology: true, useNewUrlParser: true });
+        console.log('Connected to MongoDB Atlas');
     } catch (error) {
-        console.error("Failed to connect to MongoDB", error);
-        throw error;  // Propagate the error
+        console.error('Failed to connect to MongoDB', error);
+        throw error; // Propagate the error for handling in the caller
     }
-};
+}
 
 module.exports = connectToMongoDB;
