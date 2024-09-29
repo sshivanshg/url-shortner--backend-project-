976bd731e0ec2c102fa5fb032a907879d3f135f1
sshivanshg <sshivanshg@gmail.com>
2024-09-29 21:46:01 +0530

    updated

--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-const express = require("express");
+require("dotenv").config();
+const express = require("express"); 
 const connectToMongoDB = require('./connect');
 const urlRoute = require("./routes/url");
 const app = express();
 const URL = require('./models/url');
-const PORT = 8001;
+const PORT = process.env.PORT || 8001; 
 
-connectToMongoDB(process.env.MONGODB_URI)  // Use the environment variable
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => {
-        console.error("Failed to connect to MongoDB", err);
-        process.exit(1);
-    });
+connectToMongoDB(process.env.MONGO_URL)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => {
+    console.error("Failed to connect to MongoDB", err);
+    process.exit(1);
+  });
 
 app.use(express.json());
+
 app.use("/url", urlRoute);
 
-app.get('/:shortId', async (req, res) => {
+app.get('/:shortId',async (req, res) => {
     const shortId = req.params.shortId;
     const entry = await URL.findOneAndUpdate({
-        shortId
-    }, {
-        $push: {
-            visitHistory: {
-                timestamp: Date.now(),
-            }
-        }
-    });
-    res.redirect(entry.redirectURL);
+      shortId
+    },{$push:{
+      visitHistory: {
+         timestamp: Date.now(),
+      }
+
+    },  
+   });
+   res.redirect(entry.redirectURL);
 });
 
 app.listen(PORT, () => console.log(`Server started at port:${PORT}`));
